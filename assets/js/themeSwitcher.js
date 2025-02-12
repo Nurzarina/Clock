fb3e79cde0a44cf02f4a7cbd38b5c0fce27d1179
Nurzarina <nurzarina_os@yahoo.com>
2025-02-12 22:46:13 +0800

    Added buttons to change theme for dials dynamically

--- assets/js/themeSwitcher.js
+++ assets/js/themeSwitcher.js
@@ -1,3 +1,14 @@
 function changeTheme(cssFile) {
-    document.getElementById("themeStylesheet").setAttribute("href", cssFile);
+    document.getElementById("theme-stylesheet").setAttribute("href", cssFile);
+    localStorage.setItem("selectedTheme", cssFile);                             // To save selection in localStorage.
 }
+
+// Load saved theme on page reload from localStorage.
+window.onload = function() {
+    const savedTheme = localStorage.getItem(selectedTheme);
+    if (savedTheme) {
+        document.getElementById("theme-stylesheet").setAttribute("href", savedTheme);
+    } else {
+        console.log("No saved theme detected. Loading default theme.")
+    }
+};
